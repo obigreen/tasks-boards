56aa3d1bd96478f89291800ba8de0275a28309f5
Sergey Budyakin <youshtuchka@gmail.com>
2026-08-28 23:17:00 +0300

    started task-reducer

--- src/model/task-reduser.ts
+++ src/model/task-reduser.ts
@@ -1,12 +1,12 @@
 import type {TaskStateType} from "../features/boards/board/Board.tsx";
 
 
-const taskState: TaskStateType[] = []
+const taskState: TaskStateType = {}
 
-export const taskReducer = (state: TaskStateType[] = taskState, action: Action) => {
+export const taskReducer = (state: TaskStateType = taskState, action: Action) => {
     switch (action.type) {
         case 'delete-task': {
-            return state
+            return {...state, [action.payload.boardListId]: state[action.payload.boardListId].filter(task => task.id !== action.payload.taskId)}
         }
 
         default: {
@@ -16,14 +16,14 @@ export const taskReducer = (state: TaskStateType[] = taskState, action: Action) 
 }
 
 
-export const deteleTaskAC = (boardListId: string, taskId: string) => {
+export const deleleTaskAC = (boardListId: string, taskId: string) => {
     return {
         type: 'delete-task',
         payload: {boardListId, taskId}
     } as const
 }
 
-export type deteleTaskType = ReturnType<typeof deteleTaskAC>
+export type deleleTaskType = ReturnType<typeof deleleTaskAC>
 
 
-type Action = deteleTaskType
+type Action = deleleTaskType
